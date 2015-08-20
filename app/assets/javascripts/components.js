1310c29cf5ca3a63e00dacf5564982180b055e9e
jbhatab <jbhatab@gmail.com>
2015-08-20 09:44:43 -0500

    changed stuff to defaults

---
+++ app/assets/javascripts/components.js
@@ -0,0 +1,4 @@
+//= require lodash/lodash.min
+//= require alt/dist/alt.min
+//= require initialize
+//= require_tree ./react
--- app/assets/javascripts/components.js.coffee
+++
@@ -1,4 +0,0 @@
-#= require lodash/lodash.min
-#= require alt/dist/alt.min
-#= require initialize
-#= require_tree ./react
